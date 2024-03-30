059ea47d5f04ee361cad4a696c25b493b3466bbb
Ronalds <155685973+Ronalds13424@users.noreply.github.com>
2024-03-30 15:21:52 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, UserConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()] as (UserConfig["plugins"])[],
-});
